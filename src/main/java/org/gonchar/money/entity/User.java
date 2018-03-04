0319dbcb09475d26db151b210dccab54159ab5cd
Ivan Honchar <ivan.honchar@globallogic.com>
2018-03-04 20:32:11 +0200

    feat: implement User

--- src/main/java/org/gonchar/money/entity/User.java
+++ src/main/java/org/gonchar/money/entity/User.java
@@ -1,5 +1,14 @@
 package org.gonchar.money.entity;
 
+import lombok.Data;
+
+@Data
 public class User {
 
+    /**
+     * Unique email of current user
+     */
+    private final String id;
+
+    private final String displayName;
 }
