6e6a57b321e0525961a740326dc3ad7ad0e509b1
Ivan Honchar <ivan.honchar@globallogic.com>
2018-03-04 20:28:05 +0200

    feat: implement Account

--- src/main/java/org/gonchar/money/entity/Account.java
+++ src/main/java/org/gonchar/money/entity/Account.java
@@ -1,6 +1,25 @@
 package org.gonchar.money.entity;
 
+import lombok.Data;
 
+@Data
 public class Account {
 
+    private final int id;
+
+    private final User user;
+
+    private final String displayName;
+
+    private final Currency currency;
+
+    /**
+     * Returns amount of money in this account
+     * SQL
+     * 
+     * @return money available in this account
+     */
+    public double getAmount() {
+        return 0.0;
+    }
 }
