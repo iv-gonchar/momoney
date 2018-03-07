6593a09e828e8cfd1c1665766682c394620cb222
Ivan Honchar <ivan.honchar@globallogic.com>
2018-03-07 22:21:31 +0200

    feat: improve initialization scripts

--- sql/init.sql
+++ sql/init.sql
@@ -2,9 +2,9 @@ CREATE DATABASE HomeMoney;
 
 USE HomeMoney;
 
-CREATE TABLE Currencies (id INT PRIMARY KEY, displayName VARCHAR(3));
+CREATE TABLE Currencies (id INT PRIMARY KEY, display_name VARCHAR(3));
 INSERT INTO Currencies VALUES (1, 'UAH'), (2, 'USD'), (3, 'EUR');
 
-CREATE TABLE Users (id VARCHAR(128) PRIMARY KEY, displayName VARCHAR(128));
+CREATE TABLE Users (id VARCHAR(128) PRIMARY KEY, display_name VARCHAR(128));
 
-CREATE TABLE Accounts (id INT PRIMARY KEY, uid VARCHAR(128), cid INT, displayName VARCHAR(40));
+CREATE TABLE Accounts (id INT PRIMARY KEY, user_id VARCHAR(128), currency_id INT, display_name VARCHAR(40), FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE, FOREIGN KEY (currency_id) REFERENCES Currencies(id) ON DELETE RESTRICT);
