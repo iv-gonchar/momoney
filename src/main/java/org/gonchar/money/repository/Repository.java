0de2ec205332212a8f76ff6e3d04dcf45c971ac6
Ivan Honchar <gonchar.ivan@gmail.com>
2018-04-29 19:58:40 +0300

    fix: fix parameter names in Repository

--- src/main/java/org/gonchar/money/repository/Repository.java
+++ src/main/java/org/gonchar/money/repository/Repository.java
@@ -5,11 +5,11 @@ import java.util.stream.Stream;
 
 public abstract class Repository<E> {
 
-    public abstract void add(final E user);
+    public abstract void add(final E entity);
 
-    public abstract void update(final E user);
+    public abstract void update(final E entity);
 
-    public abstract void remove(final E user);
+    public abstract void remove(final E entity);
 
     public abstract Stream<E> get(final Criteria criteria);
     
